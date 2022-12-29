2a5cfe5ceaf69cf20fabf4cff1ed15a53721616b
Mohsen Pakzad <mohsen137853@gmail.com>
2022-12-29 20:32:11 +0330

    change: use `thiserror` crate to implement app error

--- src/errors.rs
+++ src/errors.rs
@@ -1,62 +1,61 @@
 use crate::entities::payment::PaymentStatus;
 use actix_web::{http::StatusCode, Error, HttpResponse, ResponseError};
-use derive_more::Display;
 use migration::DbErr;
 use sea_orm::prelude::Decimal;
 use serde::Serialize;
+use thiserror::Error;
 
-#[derive(Debug, Display, Serialize)]
+#[derive(Debug, Error, Serialize)]
 pub enum AppError {
-    #[display(fmt = "Internal Server Error")]
+    #[error("Internal Server Error")]
     InternalServerError,
 
-    #[display(fmt = "Database Error")]
+    #[error("Database Error")]
     DataBaseError,
 
-    #[display(fmt = "User with given username already exists")]
+    #[error("User with given username already exists")]
     UsernameAlreadyFound,
 
-    #[display(fmt = "Wrong password")]
+    #[error("Wrong password")]
     WrongPassword,
 
-    #[display(fmt = "This payment isn't belongs to you")]
+    #[error("This payment isn't belongs to you")]
     PaymentIsNotBelongsToYou,
 
-    #[display(fmt = "This transaction isn't belongs to you")]
+    #[error("This transaction isn't belongs to you")]
     UserTransactionIsNotBelongsToYou,
 
-    #[display(fmt = "Payment is done or expired, payment status: {}", _0)]
+    #[error("Payment is done or expired, payment status: {0}")]
     PaymentIsDoneOrExpired(PaymentStatus),
 
-    #[display(fmt = "Payment should be done to be verified, current status: {}", _0)]
+    #[error("Payment should be done to be verified, current status: {0}")]
     PaymentShouldBeDone(PaymentStatus),
 
-    #[display(fmt = "There is no free wallet for your selected network, please try again later")]
+    #[error("There is no free wallet for your selected network, please try again later")]
     NotFreeWallet,
 
-    #[display(
-        fmt = "There is not enough balance to withdrawal, withdrawable amount for this currency is: {}",
-        _0
+    #[error(
+        "There is not enough balance to withdrawal, withdrawable amount for this currency is: {0}"
     )]
     NotEnoughBalance(Decimal),
 
     // 404s
-    #[display(fmt = "User with given id doesn't exists")]
+    #[error("User with given id doesn't exists")]
     UserNotFoundWithGivenId,
 
-    #[display(fmt = "Network with given id doesn't exists")]
+    #[error("Network with given id doesn't exists")]
     NetworkNotFoundWithGivenId,
 
-    #[display(fmt = "Fiat Currency with given id doesn't exists")]
+    #[error("Fiat Currency with given id doesn't exists")]
     FiatCurrencyNotFoundWithGivenId,
 
-    #[display(fmt = "Crypto Currency with given id doesn't exists")]
+    #[error("Crypto Currency with given id doesn't exists")]
     CryptoCurrencyNotFoundWithGivenId,
 
-    #[display(fmt = "Payment with given id doesn't exists")]
+    #[error("Payment with given id doesn't exists")]
     PaymentNotFoundWithGivenId,
 
-    #[display(fmt = "Transaction with given id doesn't exists")]
+    #[error("Transaction with given id doesn't exists")]
     UserTransactionNotFoundWithGivenId,
 }
 
